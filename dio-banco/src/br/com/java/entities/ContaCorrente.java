d021fb05246a711ff591391220498a00eded4374
jonathan.ehmke <jonathan.ehmke@partners.mv.com.br>
2022-11-27 19:29:09 -0300

    commit inicial desafio DIO

---
+++ dio-banco/src/br/com/java/entities/ContaCorrente.java
@@ -0,0 +1,14 @@
+package br.com.java.entities;
+
+public class ContaCorrente extends Conta {
+
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        imprimirInfosComuns();
+    }
+}
---
+++ dio-banco/src/br/com/java/entities/IConta.java
@@ -0,0 +1,12 @@
+package br.com.java.entities;
+
+public interface IConta {
+
+    public void sacar(double valor);
+
+    public void depositar(double valor);
+
+    public void transferir(double valor, Conta contaDestino);
+
+    public void imprimirExtrato();
+}
